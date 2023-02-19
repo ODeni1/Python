b45cb63ea9e5500e2792512ac383016916ebf741
Denisa Balinisteanu <124633739+ODeni1@users.noreply.github.com>
2023-02-19 19:36:19 +0200

    Add files via upload

---
+++ 8.py
@@ -0,0 +1,16 @@
+from utilss import find_max
+
+numbers = [1, 2, 4, 8, 3, 12]
+maxi = find_max(numbers)
+print(maxi)
+
+# utilss =
+# def find_max(numbers):
+# maxi = numbers[0]
+# for number in numbers:
+# if number > maxi:
+# maxi = number
+# return maxi
+
+
+
